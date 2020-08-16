d227ac62c1ed6b22c7850511b11d5fed904021b5
Dell pc <manvikajaiswal1995@gmail.com>
2020-08-16 19:56:02 +0530

    Password Checker Project

---
+++ PasswordChecker/checkmypass.py
@@ -0,0 +1,55 @@
+import requests
+import hashlib
+import sys
+
+
+# SHA1 hashing used
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error Fetching: {res.status_code}, check the API and try again')
+    return res
+
+
+# def read_res(response):
+#     print(response.text)
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    # print(hashes)
+    for h, count in hashes:
+        # print(h, count)
+        if h == hash_to_check:
+            return count
+    return 0
+
+
+def pwned_api_check(password):
+    # check password if it exists in API response
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    # print(first5_char, tail)
+    # print(response)
+    # return read_res(response)
+    return get_password_leaks_count(response, tail)
+
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times... you should probably change your password!')
+        else:
+            print(f'{password} was not found. Carry on!')
+    return 'done!'
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
+
+# request_api_data('123')
+# pwned_api_check('123')

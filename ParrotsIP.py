bde9b472059cd01633103c37d2fe6b072e2cf328
Dell pc <manvikajaiswal1995@gmail.com>
2020-08-21 21:15:35 +0530

    Image Processing

--- ParrotsIP.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image, ImageFilter
-
-img = Image.open('Pokedex_Images/parrots.jpg')
-# new_img= img.resize((400,400))
-img.thumbnail((400, 400))
-# new_img.save('thumbnail.jpg')
-img.save('thumbnail.jpg')
-print(img.size)
